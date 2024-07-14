1138d5559e684615616a3d9dc0b65d3f399417b0
CJSJerry <CJJerryShieh@gmail.com>
2024-07-15 01:09:54 +0200

    Fix Pydantic dependencies, now running

--- main.py
+++ main.py
@@ -3,10 +3,13 @@
 import pycaret
 from pycaret.datasets import get_data
 from pycaret.regression import *
+#from pydantic import v1 as pydantic_v1
 
 data = get_data("insurance")
 s = setup(data, target = 'charges')
 best = compare_models()
-create_api (best, 'insurance_prediction_model')
+create_api(best, 'insurance_prediction_model')
 
-# need python3.11
+# need python3.11, and older pydantic
+# pip uninstall pydantic
+# pip install pydantic==1.10.13
